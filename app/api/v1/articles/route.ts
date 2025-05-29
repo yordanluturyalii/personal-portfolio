9d359d67d6f61aab3b502bb5d0c609bddbeb0d53
yordanluturyalii <yordanluturyali@gmail.com>
2025-05-29 13:24:21 +0700

    :bug: fix: fixing bug on save to db

--- app/api/v1/articles/route.ts
+++ app/api/v1/articles/route.ts
@@ -8,15 +8,16 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
     try {
         const data = await request.json();
+        logger.info("Data: %o", data);
         const { category_id, title, read_estimation, content, is_published } = data;
 
         const article = await db.insert(articles).values({
-            category_id: String(data.category_id),
-            date: new Date(),
-            title: String(data.title),
-            read_estimation: data.read_estimation ? Number(data.read_estimation) : 0,
-            content: String(data.content),
-            is_published: Boolean(data.is_published)
+            category_id: category_id,
+            date: new Date().toISOString().split('T')[0],
+            title: title,
+            read_estimation: Number(read_estimation),
+            content: content,
+            is_published: Boolean(is_published)
         }).returning();
 
         return NextResponse.json({
